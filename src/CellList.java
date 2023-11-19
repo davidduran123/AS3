ec5162b4cf232c18b7e9b013e85e2b3de382f234
duran.enquire@gmail.com <d_duranf@live.concordia.ca>
2023-11-19 13:17:26 -0500

    Implemented deleteFromStart()

--- src/CellList.java
+++ src/CellList.java
@@ -3,22 +3,22 @@ public class CellList {
     class CellNode implements Cloneable {
     // ATTRIBUTES
         CellPhone phone; // What the node contains.
-        CellNode node; // Pointer to the next node.
+        CellNode next; // Pointer to the next node.
 
     // CONSTRUCTORS
         public CellNode () {
             phone = null;
-            node = null;
+            next = null;
         }
 
         public CellNode (CellPhone phone, CellNode node){
             this.phone = phone;
-            this.node = node;
+            this.next = node;
         }
 
         public CellNode (CellNode another){
             this.phone = another.phone;
-            this.node = another.node;
+            this.next = another.next;
         }
     // CUSTOM
         public CellNode clone() throws CloneNotSupportedException {
@@ -29,13 +29,13 @@ public class CellList {
             return phone;
         }
         public CellNode getNode() {
-            return node;
+            return next;
         }
         public void setPhone(CellPhone phone) {
             this.phone = phone;
         }
         public void setNode(CellNode node) {
-            this.node = node;
+            this.next = node;
         }
     }
 
@@ -69,6 +69,21 @@ public class CellList {
     // ACTION METHODS
     public void addToStart(CellPhone phone) {
         this.head = new CellNode(phone, this.head);
+    }
+
+    /**
+     * Method deletes the first node in the linked list if it exits.
+     * If the list is empty, then the method lets the user know.
+     */
+    public void deleteFromStart(){
+        if(this.head == null) // Scenario #1 (List in empty)
+            System.out.println("ERROR: No node to delete, list is empty!");
+        else { // Scenario #2 (List has at least one node)
+            CellNode t = this.head;
+            this.head = t.next;
+            t.next = null;
+            t = null;
+        }
     }
 
 
